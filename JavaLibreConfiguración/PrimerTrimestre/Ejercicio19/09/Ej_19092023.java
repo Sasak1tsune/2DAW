95e0f8b1d777b3c6428efff2d9ba8e8dff97c333
Sasaki <juanpapereas@gmail.com>
2023-09-22 02:50:42 +0200

    Todo hecho, arreglar cosillas

--- JavaLibreConfiguración/PrimerTrimestre/Ejercicio19/09/Ej_19092023.java
+++ JavaLibreConfiguración/PrimerTrimestre/Ejercicio19/09/Ej_19092023.java
@@ -82,6 +82,28 @@ public class Ej_19092023{
         }
         
         return numeroConsonantes;
-        //
     }
+    public static String pasarMinuscula(String cadena){
+        cadena = cadena.toLowerCase();
+        return cadena;
+    }
+    public static String pasarMayuscula(String cadena){
+        cadena = cadena.toUpperCase();
+        return cadena;
+    }
+    public class Palindromo {
+        public static boolean esPalindromo(String cadena) {
+            // Eliminar espacios en blanco y convertir a minúsculas para hacer la comparación insensible a mayúsculas y minúsculas.
+            cadena = cadena.replaceAll("\\s+", "").toLowerCase();
+            
+            int longitud = cadena.length();
+            
+            for (int i = 0; i < longitud / 2; i++) {
+                if (cadena.charAt(i) != cadena.charAt(longitud - 1 - i)) {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
 }
